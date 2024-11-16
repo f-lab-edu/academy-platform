7e758a91221a506843473be2eae4bf8f0151dd92
Joon Gyu <qkrwnsrb224@gmail.com>
2024-11-17 00:18:38 +0900

    Test: 회원가입 테스트 로직 작성
    회원 가입 테스트 로직 작성 및 application-test.yml 파일 설정 변경 진행

--- src/test/java/spring/academyPlatform/user/dao/UserRepositoryTest.java
+++ src/test/java/spring/academyPlatform/user/dao/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import spring.academyPlatform.config.AbstractIntegrationTest;
+import spring.academyPlatform.global.util.BcryptPasswordEncryptor;
 import spring.academyPlatform.user.domain.User;
 
 class UserRepositoryTest extends AbstractIntegrationTest {
@@ -17,11 +18,39 @@ class UserRepositoryTest extends AbstractIntegrationTest {
 	@Test
 	@DisplayName("사용자명으로 검색하기")
 	void testFindUser() {
-		// Arrange
+
 		User user = userRepository.findByUserName("홍길동2");
 
-		// Assert
 		assertThat(user).isNotNull();
 		assertThat(user.getUserName()).isEqualTo("홍길동2");
+	}
+
+	@Test
+	@DisplayName("회원가입 진행하기")
+	void testSaveUser() {
+
+		// given
+		String password = "123456";
+
+		BcryptPasswordEncryptor.hashPassword("123456");
+
+		User user = User.builder()
+			.userId("test_id")
+			.userPassword(password)
+			.userName("test_name")
+			.userType("student")
+			.deletedYn("Y")
+			.build();
+
+		// when
+		User savedUser = userRepository.save(user);
+
+		// then
+		assertThat(savedUser).isNotNull();
+		assertThat(savedUser.getUserName()).isEqualTo("test_name");
+		assertThat(savedUser.getUserPassword()).isEqualTo(password);
+		assertThat(savedUser.getUserType()).isEqualTo("student");
+		assertThat(savedUser.getDeletedYn()).isEqualTo("Y");
+
 	}
 }
