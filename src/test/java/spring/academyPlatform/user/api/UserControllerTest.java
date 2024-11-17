fd95296352e8b401733534b01f140ca4a7f25be2
Joon Gyu <qkrwnsrb224@gmail.com>
2024-11-17 16:37:13 +0900

    Test: 회원가입 테스트 코드 수정 및 로그인 테스트 코드 추가
    회원가입 테스트 로직 일부 수정 및 로그인 테스트 코드 추가

--- src/test/java/spring/academyPlatform/user/api/UserControllerTest.java
+++ src/test/java/spring/academyPlatform/user/api/UserControllerTest.java
@@ -3,6 +3,7 @@ package spring.academyPlatform.user.api;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import spring.academyPlatform.config.AbstractIntegrationTest;
+import spring.academyPlatform.user.dao.UserRepository;
 import spring.academyPlatform.user.dto.UserInsertParamDto;
 
 @SpringBootTest
@@ -29,8 +31,8 @@ class UserControllerTest extends AbstractIntegrationTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	// @Autowired
-	// private ObjectMapper objectMapper;
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void join_user() throws Exception {
@@ -43,27 +45,52 @@ class UserControllerTest extends AbstractIntegrationTest {
 			.userType("student")
 			.build();
 
-		mockMvc.perform(post("/api/v1/user/join-user")
-				.contentType(MediaType.APPLICATION_JSON)
+		mockMvc.perform(post("/api/v1/user/join-user").contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(dto)))
 			.andExpect(status().isOk())
-			.andDo(document("user-join", // 문서 조각 디렉토리 명
-					requestFields( // 요청 본문 필드 정보 입력
-						fieldWithPath("userId").description("User ID"),
-						fieldWithPath("userPassword").description("User Password"),
-						fieldWithPath("userName").description("User Name"),
-						fieldWithPath("userType").description("User Type")
-					),
-					responseFields( // 응답 필드 정보 입력
+			.andDo(document("join_user", // 문서 조각 디렉토리 명
+				requestFields( // 요청 본문 필드 정보 입력
+					fieldWithPath("userId").description("유저 아이디"),
+					fieldWithPath("userPassword").description("유저 비밀번호"),
+					fieldWithPath("userName").description("유저 이름"),
+					fieldWithPath("userType").description("유저 타입")),
+				responseFields( // 응답 필드 정보 입력
+					fieldWithPath("status").description("응답 상태 코드"), fieldWithPath("message").description("응답 메시지"),
+					fieldWithPath("data.userId").description("유저 아이디"),
+					fieldWithPath("data.userName").description("유저 이름"),
+					fieldWithPath("data.userType").description("유저 타입"),
+					fieldWithPath("data.createdBy").description("생성자"),
+					fieldWithPath("data.deletedYn").description("삭제여부"))));
+	}
+
+	@Test
+	void login_user() throws Exception {
+
+		// 사용자 생성
+		UserInsertParamDto dto = UserInsertParamDto.builder()
+			.userId("test9")
+			.userPassword("password")
+			.userName("test_name")
+			.userType("student")
+			.build();
+
+		mockMvc.perform(post("/api/v1/user/join-user").contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(dto))).andExpect(status().isOk());
+
+		this.mockMvc.perform(post("/api/v1/user/login-user")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("userId", "test9")
+				.param("password", "password"))
+			.andExpect(status().isOk())
+			.andDo(document("login_user", formParameters(parameterWithName("userId").description("User ID"),
+						parameterWithName("password").description("User Password")),
+					responseFields( // 응답 필드 명세 추가
 						fieldWithPath("status").description("응답 상태 코드"),
 						fieldWithPath("message").description("응답 메시지"),
-						fieldWithPath("data.userId").description("User ID"),
-						fieldWithPath("data.userName").description("User Name"),
-						fieldWithPath("data.userType").description("User Type"),
-						fieldWithPath("data.createdBy").description("Created By"),
-						fieldWithPath("data.deletedYn").description("Deleted Yn")
+						fieldWithPath("data").description("로그인 성공 여부")
 					)
 				)
 			);
 	}
+
 }
