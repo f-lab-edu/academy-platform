e0ab0b727ed9fa1d5767bebd2dbde4c2e509f189
Joon Gyu <qkrwnsrb224@gmail.com>
2024-11-17 16:43:40 +0900

    Fix: 버그 수정
    리포지토리에 id type이 잘못된 오류 수정
    
    컨트롤러 메서드 명이 부적절하여 수정

--- src/main/java/spring/academyPlatform/user/api/UserController.java
+++ src/main/java/spring/academyPlatform/user/api/UserController.java
@@ -5,8 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +26,7 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/join-user")
-	public ResponseEntity<Message> getUser(@Valid @RequestBody UserInsertParamDto dto) {
+	public ResponseEntity<Message> insertUser(@Valid @RequestBody UserInsertParamDto dto) {
 		try {
 			UserInsertResponseDto result = userService.insertUser(dto);
 			return ResponseEntity.status(HttpStatus.OK)
