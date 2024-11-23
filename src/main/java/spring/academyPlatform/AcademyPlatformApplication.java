8ad51e629cd16b5921906b1e7f1c8f284d6cb7ec
Joon Gyu <qkrwnsrb224@gmail.com>
2024-11-23 18:25:41 +0900

    Feat: jacoco 설정
    테스트 커버리지 측정을 위한 jacoco 설정 진행

--- src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
+++ src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import lombok.Generated;
+
 @EnableJpaAuditing
 @SpringBootApplication
+@Generated
 public class AcademyPlatformApplication {
 
 	public static void main(String[] args) {
