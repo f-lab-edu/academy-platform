a11dd83b2ababe7d93897c9fcc093f441f3607be
Joon Gyu <qkrwnsrb224@gmail.com>
2024-11-30 17:51:32 +0900

    Refactor: UserService 클래스 리펙터링
    builder에서 Mapper 사용으로 변경 및 사용하지 않는 로직 삭제

--- src/main/java/spring/academyPlatform/user/application/UserService.java
+++ src/main/java/spring/academyPlatform/user/application/UserService.java
@@ -5,42 +5,30 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import spring.academyPlatform.global.mapper.UserMapper;
 import spring.academyPlatform.global.util.BcryptPasswordEncryptor;
 import spring.academyPlatform.user.dao.UserRepository;
 import spring.academyPlatform.user.domain.User;
-import spring.academyPlatform.user.dto.UserInsertParamDto;
-import spring.academyPlatform.user.dto.UserInsertResponseDto;
+import spring.academyPlatform.user.dto.UserCreateRequest;
+import spring.academyPlatform.user.dto.UserCreateResponse;
+import spring.academyPlatform.user.mapper.UserMapper;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional
 public class UserService {
 
 	private final UserRepository userRepository;
 
-	public UserInsertResponseDto insertUser(UserInsertParamDto dto) {
+	@Transactional
+	public UserCreateResponse createUser(UserCreateRequest dto) {
 
-		String hashPassword = BcryptPasswordEncryptor.hashPassword(dto.getUserPassword());
+		String encodedPassword = BcryptPasswordEncryptor.hashPassword(dto.getUserPassword());
 
-		User user = User.builder()
-			.userId(dto.getUserId())
-			.userType(dto.getUserType())
-			.userName(dto.getUserName())
-			.userPassword(hashPassword)
-			.createdBy(dto.getUserName())
-			.deletedYn("Y")
-			.build();
+		User user = UserMapper.fromDto(dto, encodedPassword);
+		log.info("User created: {}", user.getDeletedYn().toString());
 		User savedUser = userRepository.save(user);
 
 		return UserMapper.fromEntity(savedUser);
 	}
 
-/*	public UserDto findUser(Long id) {
-
-		Optional<User> user = userRepository.findById(id);
-
-		return null;
-	}*/
 }
