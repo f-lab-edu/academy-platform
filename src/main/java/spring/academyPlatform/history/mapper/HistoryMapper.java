ba6a4a42946188107e31cdd988df918089f62e93
Joon Gyu <qkrwnsrb224@gmail.com>
2024-12-07 03:17:41 +0900

    Feat: Histoy 삭제를 위한 mapper 추가
    History 삭제를 위한 Mapper 추가함.

--- src/main/java/spring/academyPlatform/history/mapper/HistoryMapper.java
+++ src/main/java/spring/academyPlatform/history/mapper/HistoryMapper.java
@@ -1,7 +1,9 @@
 package spring.academyPlatform.history.mapper;
 
+import spring.academyPlatform.global.model.YnCode;
 import spring.academyPlatform.history.domain.History;
 import spring.academyPlatform.history.dto.HistoryCreateRequest;
+import spring.academyPlatform.history.dto.HistoryPatchResponse;
 import spring.academyPlatform.history.dto.HistorySearchResponse;
 
 public class HistoryMapper {
@@ -16,8 +18,32 @@ public class HistoryMapper {
 			.build();
 	}
 
+	public static History from(HistoryPatchResponse dto) {
+		return History.builder()
+			.id(dto.getId())
+			.tableName(dto.getTableName())
+			.tableId(dto.getTableId())
+			.changedData(dto.getChangedData())
+			.operationType(dto.getOperationType())
+			.createdBy(dto.getCreatedBy())
+			.deletedYn(YnCode.valueOf(dto.getDeletedYn()))
+			.build();
+	}
+
 	public static HistorySearchResponse fromEntity(History entity) {
 		return HistorySearchResponse.builder()
+			.id(entity.getId())
+			.tableName(entity.getTableName())
+			.tableId(entity.getTableId())
+			.operationType(entity.getOperationType())
+			.createdBy(entity.getCreatedBy())
+			.changedData(entity.getChangedData())
+			.deletedYn(String.valueOf(entity.getDeletedYn()))
+			.build();
+	}
+
+	public static HistoryPatchResponse fromPatchResponse(History entity) {
+		return HistoryPatchResponse.builder()
 			.id(entity.getId())
 			.tableName(entity.getTableName())
 			.tableId(entity.getTableId())
