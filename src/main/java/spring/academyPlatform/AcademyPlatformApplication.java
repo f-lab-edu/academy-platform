4dc795575b05b2fee9169b58b76c24bd66dd558b
Joon Gyu <qkrwnsrb224@gmail.com>
2024-12-09 22:06:39 +0900

    Chore: 어노테이션 설명추가
    설명이 누락된 어노테이션의 설명을 추가함

--- src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
+++ src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
@@ -8,7 +8,7 @@ import lombok.Generated;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@Generated
+@Generated // jacoco에서 해당 어노테이션이 붙은 클래스의 테스트 커버리지를 측정하지 않도록 합니다.
 public class AcademyPlatformApplication {
 
 	public static void main(String[] args) {
