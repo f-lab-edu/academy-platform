5c4e51a977f2df8a984309d07cfdf7662d33b758
Joon Gyu <qkrwnsrb224@gmail.com>
2024-12-16 23:22:16 +0900

    Refactor: History mapper 수정
    update 작업에 사용되었던 mapper 제거 및 history 등록에 사용되는 mapper 등록

--- src/main/java/spring/academyPlatform/history/mapper/HistoryMapper.java
+++ src/main/java/spring/academyPlatform/history/mapper/HistoryMapper.java
@@ -1,32 +1,19 @@
 package spring.academyPlatform.history.mapper;
 
-import spring.academyPlatform.global.model.YnCode;
 import spring.academyPlatform.history.domain.History;
 import spring.academyPlatform.history.dto.HistoryCreateRequest;
-import spring.academyPlatform.history.dto.HistoryPatchResponse;
+import spring.academyPlatform.history.dto.HistoryCreateResponse;
 import spring.academyPlatform.history.dto.HistorySearchResponse;
 
 public class HistoryMapper {
 
-	public static History from(HistoryCreateRequest dto) {
+	public static History from(HistoryCreateRequest dto, String changedData) {
 		return History.builder()
 			.tableName(dto.getTableName())
 			.tableId(dto.getTableId())
-			.changedData(dto.getChangedData())
+			.changedData(changedData)
 			.operationType(dto.getOperationType())
 			.createdBy(dto.getCreatedBy())
-			.build();
-	}
-
-	public static History from(HistoryPatchResponse dto) {
-		return History.builder()
-			.id(dto.getId())
-			.tableName(dto.getTableName())
-			.tableId(dto.getTableId())
-			.changedData(dto.getChangedData())
-			.operationType(dto.getOperationType())
-			.createdBy(dto.getCreatedBy())
-			.deletedYn(YnCode.valueOf(dto.getDeletedYn()))
 			.build();
 	}
 
@@ -42,8 +29,8 @@ public class HistoryMapper {
 			.build();
 	}
 
-	public static HistoryPatchResponse fromPatchResponse(History entity) {
-		return HistoryPatchResponse.builder()
+	public static HistoryCreateResponse fromCreateResponse(History entity) {
+		return HistoryCreateResponse.builder()
 			.id(entity.getId())
 			.tableName(entity.getTableName())
 			.tableId(entity.getTableId())
@@ -51,6 +38,8 @@ public class HistoryMapper {
 			.createdBy(entity.getCreatedBy())
 			.changedData(entity.getChangedData())
 			.deletedYn(String.valueOf(entity.getDeletedYn()))
+			.createdAt(entity.getCreatedAt())
+			.createdBy(entity.getCreatedBy())
 			.build();
 	}
 
