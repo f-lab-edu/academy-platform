c4ffaa29a75127d09a752c9528a34fb146efd374
Joon Gyu <qkrwnsrb224@gmail.com>
2024-12-22 17:29:51 +0900

    Feat: redis에 세션 저장기능 추가
    로그인시 세션을 redis에 저장하도록 변경함

--- src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
+++ src/main/java/spring/academyPlatform/AcademyPlatformApplication.java
@@ -3,12 +3,14 @@ package spring.academyPlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import lombok.Generated;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @Generated
+@EnableRedisHttpSession // redis에 세션을 저장하기 위한 설정
 public class AcademyPlatformApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/spring/academyPlatform/global/config/redis/RedisConfig.java
@@ -0,0 +1,58 @@
+package spring.academyPlatform.global.config.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.password}")
+	private String password;
+
+	@Value("${spring.data.redis.port}")
+	private int port;
+
+	@Bean
+	public LettuceConnectionFactory lettuceConnectionFactory() {
+		final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+		redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	// redis에 데이터를 직렬화하여 저장하기 위한 설정입니다.
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+
+		// key는 StringRedisSerializer 사용
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new StringRedisSerializer());
+
+		// value는 JSON Serializer 사용
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+		template.afterPropertiesSet();
+		return template;
+	}
+
+	// Spring Session이 사용할 기본 RedisSerializer 설정
+	@Bean(name = "springSessionDefaultRedisSerializer")
+	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+		return new GenericJackson2JsonRedisSerializer();
+	}
+
+}
