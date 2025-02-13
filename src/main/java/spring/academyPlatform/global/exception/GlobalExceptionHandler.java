7e35e3effe4305c78f37c0344e52009356a3e81c
Joon Gyu <qkrwnsrb224@gmail.com>
2025-02-14 01:05:21 +0900

    Fix : Global Exception 로직 수정 Global Exception return 값 수정 진행

--- src/main/java/spring/academyPlatform/global/exception/GlobalExceptionHandler.java
+++ src/main/java/spring/academyPlatform/global/exception/GlobalExceptionHandler.java
@@ -16,16 +16,18 @@ public class GlobalExceptionHandler {
 	// @Pattern 어노테이션을 사용하여 @RequestParam을 검증할 때, 위반 시 발생하는 예외는 ConstraintViolationException 로 받는다.
 	@ExceptionHandler(ConstraintViolationException.class)
 	protected ResponseEntity<String> constraintViolationException(ConstraintViolationException ex) {
+		log.error(ex.getMessage());
 		return ResponseEntity
 			.status(HttpStatus.BAD_REQUEST)
-			.body(ex.getMessage());
+			.body("파라미터 검증에서 오류가 발생하였습니다.");
 	}
 
 	// @Valid 통해 검증시 위반사항이 발생하여 이를 받는 예외처리
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<String> processValidationError(MethodArgumentNotValidException e) {
+		log.error(e.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(e.getMessage());
+			.body("유효성 검사에서 문제가 발생하엿습니다.");
 	}
 
 	/**
@@ -33,8 +35,9 @@ public class GlobalExceptionHandler {
 	 */
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<String> exception(Exception e) {
+		log.error(e.getMessage());
 		return ResponseEntity
 			.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(e.getMessage());
+			.body("서버 측 오류가 발생하였습니다.");
 	}
 }
