4c2cf8991ea1e54b6a834ca83137b460d302a93e
Joon Gyu <qkrwnsrb224@gmail.com>
2025-02-24 00:03:19 +0900

    Fix : 게시글 생성 후 return 값 수정 return 값에서 불필요한 정보를 제거함

--- src/main/java/spring/academyPlatform/qa_board/dto/QaBoardCreateResponse.java
+++ src/main/java/spring/academyPlatform/qa_board/dto/QaBoardCreateResponse.java
@@ -13,22 +13,18 @@ public class QaBoardCreateResponse {
 	private String title;
 	private String post;
 	private LocalDateTime createdAt;
-	private LocalDateTime modifiedAt;
 	private String createdBy;
-	private String modifiedBy;
 	private YnCode deletedYn;
 
 	@Builder(toBuilder = true)
 	public QaBoardCreateResponse(Long boardId, String userId, String title, String post, LocalDateTime createdAt,
-		LocalDateTime modifiedAt, String createdBy, String modifiedBy, YnCode deletedYn) {
+		String createdBy, YnCode deletedYn) {
 		this.boardId = boardId;
 		this.userId = userId;
 		this.title = title;
 		this.post = post;
 		this.createdAt = createdAt;
-		this.modifiedAt = modifiedAt;
 		this.createdBy = createdBy;
-		this.modifiedBy = modifiedBy;
 		this.deletedYn = deletedYn;
 	}
 }
